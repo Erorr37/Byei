634620b95af4d71e4e2cd23ab039968a5b03a244
Erorr37 <147059073+Erorr37@users.noreply.github.com>
2023-10-18 20:32:24 +0700

    Add files via upload

---
+++ BYERR.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+
+red='\033[0;31m'
+hijau='\033[0;32m'
+kuning='\033[0;33m'
+biru='\033[0;34m'
+ungu='\033[0;35m'
+cyan='\033[0;36m'
+putih='\033[0;37m'
+brown='\e[0;33m'
+
+clear
+
+echo "           ',."
+echo "             ....'',:okd"
+echo "                   ..;;   ."
+echo "                          l'."
+echo "                        .x;      l,;.."
+echo "                       xo            oo"
+echo "                      .M               .'"
+echo "                       M.  Kali Linux"
+echo "                        o:"
+echo "                          .lc;,.."
+echo "                                    ;o  ."
+echo "                                       '"
+echo ""
+echo "                                                      ."
+echo -e "            ${hijau}=====${biru}BYERR${hijau}====="
+echo "=============================================="
+read -p "host              : "
+sleep 1
+echo    "Methodd           :https2"
+sleep 1
+read -p "port              : "
+sleep 1
+read -p "Time              : "
+sleep 1
+read -p "Threads           : [100/"
+sleep 1
+read -p "Cooldown          : "
+sleep 1
+echo    "Loaded Bot        :914046"
+sleep 1
+read -p "NetWork VIP       :[true/false] "
+sleep 1
+echo "=============================================="
+echo -e "${cyan}»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»"
+sleep 1
+echo -e "${hijau}Attack has sent ${kuning}Succesfully To All Server"
+echo -e "${cyan}»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»"
